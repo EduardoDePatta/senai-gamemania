0851c4c97806e211c8be567cb63f5432d56a7eda
Eduardo Giacometti De Patta <106563398+EduardoDePatta@users.noreply.github.com>
2022-08-31 17:18:00 -0300

    Add files via upload

---
+++ Game Mania/script/login.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    $('#emailInput').click( function() {
+        $('#spanEmailHelp').text('Digite seu E-mail')
+      })
+
+    $('#passwordInput').click( function() {
+        $('#spanPasswordHelp').text('Digite sua senha')
+    })
+})
+
+
+
